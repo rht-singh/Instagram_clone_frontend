1cf86f95ebda175e99fabef920619fb1324a4347
Rohit <rohit.singh@roxiler.com>
2022-03-20 09:46:44 +0530

    clone

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,16 @@
-import React from 'react'
-import * as BsIcon from 'react-icons/bs'
-const Lstyle = ()=>{
-    return (
-        <style>
-            {`
+import React, { useState } from "react";
+import axios from "axios";
+import * as BsIcon from "react-icons/bs";
+const Lstyle = () => {
+  return (
+    <style>
+      {`
                 @import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
                 @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
                 .main-container{
                     width:100%;
                     height: 100vh;
+                    overflow:hidden;
                 }
                 
                 .second-container{
@@ -32,8 +34,8 @@ const Lstyle = ()=>{
                 .form input{
                     width: 100%;
                     border: 1px solid rgb(158, 157, 157);
-                    padding: 5px;
-                    height: 44px;
+                    padding: 10px;
+                    height: 48px;
                     margin-bottom: 15px;
                     border-radius: 5px;
                 }
@@ -41,13 +43,14 @@ const Lstyle = ()=>{
                     outline: none;
                 }
                 .form button{
-                    background-color: #0D6EFD;
+                    background-color: #3593eb;
                     width: 100%;
                     height: 48px;
                     text-align: center;
                     padding: 5px 0px;
                     border: none;
                     color: white;
+                    font-weight:800;
                     margin-bottom: 10px;
                     border-radius: 5px;
                 }
@@ -62,6 +65,9 @@ const Lstyle = ()=>{
                 .form-help a{
                     text-decoration: none;
                     color: rgb(8, 8, 44);
+                    font-size:14px;
+                    margin-top: 15px;
+                    font-weight:600;
                 }
                 .form-help a span{
                     color: gray;
@@ -72,6 +78,7 @@ const Lstyle = ()=>{
                     width: 100%;
                     justify-content: center;
                     color: gray;
+                    margin-top: 20px;
                     margin-bottom: 15px;
                     font-weight: 600;
                     font-family: 'Roboto', sans-serif;
@@ -103,7 +110,8 @@ const Lstyle = ()=>{
                 .bottom-signup{
                     display: flex;
                     justify-content: center;
-                    margin-top: 5%;
+                    position:relative;
+                    top:-50px;
                     border-top: 1px solid rgb(158, 158, 158);
                     padding-top: 14px;
                     font-family: 'Roboto', sans-serif;
@@ -117,38 +125,110 @@ const Lstyle = ()=>{
                 .bottom-signup a span{
                     color: gray;
                 }
+                .fb-size{
+                    height:50px;
+                }
+                img{
+                    position:relative;
+                    left:10px;
+                }
+                .error_color{
+                    color:red;
+                }
             `}
-        </style>
-    )
-}
+    </style>
+  );
+};
 const Login = () => {
+  const [formData, setFormData] = useState({
+    mail: "",
+    pass: "",
+  });
+  const [error, setError] = useState("");
+  function handleSubmit(e) {
+    const { mail, pass } = formData;
+
+    if (mail.length && pass.length) {
+      const url = `https://instagramclonebackground.herokuapp.com/api/v1/login?mail=${formData.mail}&pass=${pass}`;
+      axios
+        .get(url)
+        .then((resp) => {
+          if (resp.data.success) {
+            window.location.assign("https://intalgo.vercel.app");
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else setError("Required");
+  }
   return (
     <>
-        <div className="main-container">
-            <div className="second-container">
-                <h1>Instagram</h1>
-                <form action="" className='form'>
-                    <input type="text" placeholder='Phone number, email or usrname'/>
-                    <input type="password"  placeholder='password'/>
-                    <button type='submit' className='button'>Login</button>
-                </form>
-                <div className="form-help">
-                    <a href="/"><span>Forget your login details?</span> Get help logging in.</a>
-                </div>
-                <div className="two-line">
-                    <hr className='one'/> Or <hr className='two'/>
-                </div>
-                <div className="help-border">
-                    <a href="/"><span><BsIcon.BsFacebook/></span> Log in with Facebook</a>
-                </div>
-            </div>
-            <div className="bottom-signup">
-                <a href="/"><span>Don't have an account?</span> Sign up.</a>
-            </div>
+      <div className="main-container p-2">
+        <div className="second-container">
+          <div>
+            <img
+              className="w-75 h-75 mx-4 mb-4"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/2560px-Instagram_logo.svg.png"
+            />
+          </div>
+          <div className="form">
+            <input
+              className="bg bg-light"
+              type="text"
+              placeholder="Phone number, email or usrname"
+              required
+              onChange={(e) => {
+                setFormData({
+                  ...formData,
+                  mail: e.target.value,
+                });
+              }}
+            />
+            <p className="error_color">{error}</p>
+            <input
+              className="bg bg-light"
+              type="password"
+              placeholder="Password"
+              required
+              onChange={(e) => {
+                setFormData({
+                  ...formData,
+                  pass: e.target.value,
+                });
+              }}
+            />
+            <p className="error_color">{error}</p>
+            <button type="submit" className="button" onClick={handleSubmit}>
+              Log In
+            </button>
+          </div>
+          <div className="form-help">
+            <a className="" href="/">
+              <span>Forget your login details?</span> Get help logging in.
+            </a>
+          </div>
+          <div className="two-line">
+            <hr className="one" /> OR <hr className="two" />
+          </div>
+          <div className="help-border">
+            <a href="/">
+              <span>
+                <BsIcon.BsFacebook className="fb-size" />
+              </span>{" "}
+              Log in with Facebook
+            </a>
+          </div>
         </div>
-        <Lstyle/>
+        <div className="bottom-signup p-3">
+          <a style={{ fontWeight: 800 }} href="/">
+            <span>Don't have an account?</span> Sign up.
+          </a>
+        </div>
+      </div>
+      <Lstyle />
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
